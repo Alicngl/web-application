8bb6cbbf36dd91383cda66ef008bb7589373b2e9
Ali Cangül <cangulali1@gmail.com>
2023-01-26 02:22:28 +0300

    client home,auth page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth";
 import Home from "./pages/Home";
 function App() {
   return (
@@ -6,6 +7,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </BrowserRouter>
     </div>
